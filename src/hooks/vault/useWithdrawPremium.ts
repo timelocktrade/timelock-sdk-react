bbcc3fa3968dad877ecb5cc191491b207730716f
Apoorv <github@libkakashi.dev>
2026-01-18 01:34:13 +0530

    withdraw premium should take arrays intead of individual values

--- src/hooks/vault/useWithdrawPremium.ts
+++ src/hooks/vault/useWithdrawPremium.ts
@@ -1,19 +1,29 @@
-import type {Address} from 'viem';
+import {zeroAddress, type Address} from 'viem';
 import {useWriteContract} from 'wagmi';
 import {singleOwnerVaultAbi} from '~/abis/singleOwnerVault';
 
 export const useWithdrawPremium = (vaultAddr: Address | undefined) => {
   const {mutateAsync, ...rest} = useWriteContract();
 
-  const withdrawPremium = (asset: Address) => {
+  const withdrawPremium = (
+    assets: Address[],
+    amounts: bigint[],
+    recipient: Address,
+  ) => {
     if (!vaultAddr) {
       throw new Error('Vault address not provided');
+    }
+    if (!assets.length || !amounts.length || assets.length !== amounts.length) {
+      throw new Error('Invalid assets or amounts');
+    }
+    if (recipient === zeroAddress) {
+      throw new Error('Invalid recipient address');
     }
     return mutateAsync({
       address: vaultAddr,
       abi: singleOwnerVaultAbi,
       functionName: 'withdrawTokens',
-      args: [[asset]],
+      args: [assets, amounts, recipient],
     });
   };
   return {withdrawPremium, ...rest};
